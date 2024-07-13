eda96fc7eb0227ad88dee2cf495427c849a76ed8
francolloveras <francorlloveras@gmail.com>
2024-07-12 22:30:59 -0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
+  },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+]
