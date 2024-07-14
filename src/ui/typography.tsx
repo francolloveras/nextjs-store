30d2cde5dfd8eda05dec05ad7e2b413679945a1a
francolloveras <francorlloveras@gmail.com>
2024-07-13 21:14:24 -0300

    Improve Typography UI component

--- src/ui/typography.tsx
+++ src/ui/typography.tsx
@@ -2,21 +2,31 @@ import clsx from 'clsx'
 
 import Icon, { IconProps, IconValues } from '@/ui/icon'
 
-interface TypographyProps extends Omit<React.HTMLProps<HTMLElement>, 'as'> {
-  as: React.ElementType
+interface TypographyProps<T extends React.ElementType> {
+  as: T
   icon?: IconValues | IconProps
-  children: string | number | React.ReactElement
+  variant?: 'transparent-button'
+  children?: string | number | React.ReactElement
 }
 
-export default function Typography({ as: Tag, icon, children, ...rest }: TypographyProps) {
+export default function Typography<T extends React.ElementType>({
+  as: Tag,
+  icon,
+  variant,
+  children,
+  ...rest
+}: TypographyProps<T> & Omit<React.ComponentPropsWithRef<T>, keyof TypographyProps<T>>) {
   const iconProps = icon ? (typeof icon === 'string' ? { icon } : { ...icon }) : undefined
 
   return (
+    // @ts-expect-error Error type
     <Tag
       {...rest}
       className={clsx(
         {
-          'inline-flex align-middle mb-px items-center gap-x-1': icon
+          'inline-flex align-middle mb-px items-center gap-x-1': icon,
+          'hover:bg-neutral-800 rounded-md text-neutral-400 p-1.5 transition-colors duration-200 hover:text-white':
+            variant === 'transparent-button'
         },
         [rest.className]
       )}
