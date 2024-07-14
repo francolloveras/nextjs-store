1ba9e63c0d457604a97eae3a493391e92e4951cb
francolloveras <francorlloveras@gmail.com>
2024-07-14 00:03:08 -0300

    Add Header background in hover

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import Nav from '@/components/nav'
 
 export default function Header() {
   return (
-    <header className="mx-auto flex max-w-6xl items-center justify-between py-4">
-      <Link href="/" className="text-2xl font-semibold uppercase tracking-wider">
+    <header className="flex w-screen items-center justify-between px-96 py-4 transition-colors duration-300 hover:bg-[#121212]/70">
+      <Link href="/" className="text-2xl font-semibold uppercase tracking-wider text-white">
         NextJS Store
       </Link>
       <Nav />
