0864ffeabb9614b4fefdf27596e1af227ef95612
francolloveras <francorlloveras@gmail.com>
2024-07-14 20:47:39 -0300

    Add ESRB ratings constant data

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -7,3 +7,30 @@ export const STEAM_API_PARAMS = {
 export const PATHS = {
   GAMES: '/games'
 } as const
+
+export const ESRB_RATINGS: Record<string, { image: string; title: string }> = {
+  m: {
+    image: '/esrb-ratings/m.svg',
+    title: 'Mature 17+'
+  },
+  ao: {
+    image: '/esrb-ratings/ao.svg',
+    title: 'Adults Only 18+'
+  },
+  e: {
+    image: '/esrb-ratings/e.svg',
+    title: 'Everyone'
+  },
+  e10: {
+    image: '/esrb-ratings/e10.svg',
+    title: 'Everyone 10+'
+  },
+  rp: {
+    image: '/esrb-ratings/rp.svg',
+    title: 'Rating Pending'
+  },
+  t: {
+    image: '/esrb-ratings/t.svg',
+    title: 'Teen'
+  }
+} as const
