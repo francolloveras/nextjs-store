bdfcd103e14126fdba3ca398137a0f1c186cea7c
francolloveras <francorlloveras@gmail.com>
2024-07-15 21:56:01 -0300

    Improve Footer text colors

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,14 @@ import Typography from '@/ui/typography'
 export default function footer() {
   return (
     <footer className="mb-4 flex h-40 flex-col items-center justify-end">
-      <h3 className="mb-1">
+      <h3 className="mb-1 text-neutral-300">
         Created by Franco Lloveras
         <span className="mx-2">&bull;</span>
         <Typography
           as="a"
           href="https://github.com/francolloveras/nextjs-store"
           target="_blank"
-          className="hover:text-red-400"
+          className="hover:text-white hover:underline"
           icon={{
             icon: 'github',
             className: 'text-sm'
@@ -21,7 +21,7 @@ export default function footer() {
           View code source
         </Typography>
       </h3>
-      <div className="text-pretty text-center text-sm text-neutral-300">
+      <div className="text-pretty text-center text-sm text-neutral-500">
         <p>Disclaimer: This page is demonstrative and for educational purposes only.</p>
         <p>
           All game information shown comes from{' '}
@@ -31,9 +31,9 @@ export default function footer() {
             referrerPolicy="no-referrer"
             target="_blank"
           >
-            Steam
-          </Link>{' '}
-          servers, credits to them.
+            Steam servers
+          </Link>
+          , credits to them.
         </p>
       </div>
     </footer>
