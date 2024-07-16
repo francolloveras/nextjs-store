4cde526f0cf01ac4a5fbb7894ad6dce607bf71cf
francolloveras <francorlloveras@gmail.com>
2024-07-15 22:04:07 -0300

    Improve temporal header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,10 @@ import Nav from '@/components/nav'
 export default function Header() {
   return (
     <header className="flex w-screen items-center justify-between px-96 py-4 transition-colors duration-300 hover:bg-[#121212]/70">
-      <Link href="/" className="text-2xl font-semibold uppercase tracking-wider text-white">
+      <Link
+        href="/"
+        className="text-3xl font-semibold tracking-wide text-neutral-200 hover:text-white"
+      >
         NextJS Store
       </Link>
       <Nav />
