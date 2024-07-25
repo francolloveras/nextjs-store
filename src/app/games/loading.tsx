64165c892a84f41ccd583c61e562e2895b9f8f58
francolloveras <francorlloveras@gmail.com>
2024-07-24 21:31:05 -0300

    Add loading logic to games path

---
+++ src/app/games/loading.tsx
@@ -0,0 +1,14 @@
+import { steamGameIds } from '@/lib/data'
+import SkeletonGameCard from '@/ui/skeleton-game-card'
+
+export default function Loading() {
+  return (
+    <main className="mx-auto mt-12 w-11/12">
+      <section className="grid gap-x-4 gap-y-12 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+        {steamGameIds.map((id) => (
+          <SkeletonGameCard key={id} />
+        ))}
+      </section>
+    </main>
+  )
+}
