d9978c928fc6e532eb638866c1f033ab1c245858
francolloveras <francorlloveras@gmail.com>
2024-08-06 19:11:58 -0300

    Improve Game Card UI component

--- src/ui/game-card.tsx
+++ src/ui/game-card.tsx
@@ -9,26 +9,27 @@ interface GameCardProps {
   name: string
   image: string
   price: GamePriceProps
+  type?: number
 }
 
-export default function GameCard({ id, name, image, price }: GameCardProps) {
+export default function GameCard({ id, name, image, price, type }: GameCardProps) {
   return (
-    <Link
-      href={`${PATHS.GAMES}/${id}`}
-      className="group transition-transform duration-300 ease-in-out hover:-translate-y-2"
-    >
+    <Link href={`${PATHS.GAMES}/${id}`} className="group">
       <Image
         src={image}
-        alt={`Picture for ${name}`}
+        alt={`${name} game picture`}
         width={460}
         height={215}
-        className="aspect-video rounded-xl opacity-80 transition-opacity group-hover:opacity-100"
+        className="aspect-video rounded-md opacity-80 transition-opacity group-hover:opacity-100"
         aria-disabled
       />
-      <div className="mt-2 flex flex-col gap-y-1">
-        <h3 className="text-pretty text-lg leading-snug text-neutral-300 group-hover:text-white">
-          {name}
-        </h3>
+      <div className="ml-0.5 mt-2">
+        {type !== undefined && (
+          <p className="text-sm text-neutral-600">
+            {type === 0 ? 'Base game' : type === 1 ? 'Extension package' : 'Unknown'}
+          </p>
+        )}
+        <h3 className="text-pretty text-lg text-neutral-300 group-hover:text-white">{name}</h3>
         <Price {...price} />
       </div>
     </Link>
