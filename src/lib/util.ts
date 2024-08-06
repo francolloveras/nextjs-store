5f99795871f7708f07f00a551c0afe5e7bedb847
francolloveras <francorlloveras@gmail.com>
2024-08-06 19:12:17 -0300

    Add new formats methods

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,7 +2,15 @@ export const uppercaseFirstLetter = (text: string) => {
   return text.charAt(0).toUpperCase() + text.slice(1)
 }
 
-export const formatPrice = (amount: number, locale: string = 'en-US', currency: string = 'USD') => {
+export const formatPrice = ({
+  amount,
+  locale = 'en-US',
+  currency = 'USD'
+}: {
+  amount: number
+  locale?: string
+  currency?: string
+}) => {
   const adjustedAmount = amount / 100
 
   return new Intl.NumberFormat(locale, {
@@ -11,4 +19,18 @@ export const formatPrice = (amount: number, locale: string = 'en-US', currency: 
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   }).format(adjustedAmount)
+}
+
+export const formatDate = ({
+  seconds,
+  locale = 'en-US',
+  options
+}: {
+  seconds: number
+  locale?: string
+  options?: Intl.DateTimeFormatOptions
+}) => {
+  const date = new Date(seconds * 1000)
+
+  return new Intl.DateTimeFormat(locale, options).format(date)
 }
